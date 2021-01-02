ca30b295497bf48d3749a3131a92b38b915a6a2a
NilDuMaN <44227115+NilDuMaN@users.noreply.github.com>
2021-01-02 14:33:41 +0300

    Update simulated.py

--- simulated.py
+++ simulated.py
@@ -1,103 +1,100 @@
-## Generate a contour plot
-# Import some other libraries that we'll need
-# matplotlib and numpy packages must also be installed
+## ni.duman
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 import math
 
-# define objective function
+
+# fonkisyonu tanımlama
 def f(x):
     x1 = x[0]
     x2 = x[1]
     obj = (1 + (x1 + x2 + 1)**2 * (19 - 14*x1 + 3*(x1**2) - 14*x2 + 6*x1*x2 + 3*(x2**2))) * (30 + (2*x1 - 3*x2)**2 * (18 - 32*x1 + 12*(x1**2) + 48*x2 - 36*x1*x2 + 27*(x2**2)))
     return obj
 
-# Start location
+# aralık
 x_start = [0.8, -0.5]
 
 
-##################################################
-# Simulated Annealing
-##################################################
-# Number of cycles
+# Simulated Kodu
+
+# Düngü sayısı
 n = 30
-# Number of trials per cycle
+# Döngü başına deneme sayısı
 m = 75
-# Number of accepted solutions
+
 na = 0.0
-# Probability of accepting worse solution at the start
+# en kötü çözüm başlangıç
 p1 = 0.7
-# Probability of accepting worse solution at the end
+# En kötü çözüm bitiş
 p50 = 0.001
-# Initial temperature
+# başlangıç sıcaklığı
 t1 = -1.0/math.log(p1)
-# Final temperature
+# final sıcaklığı
 t50 = -1.0/math.log(p50)
-# Fractional reduction every cycle
+
 frac = (t50/t1)**(1.0/(n-1.0))
-# Initialize x
+
 x = np.zeros((n+1,2))
 x[0] = x_start
 xi = np.zeros(2)
 xi = x_start
 na = na + 1.0
-# Current best results so far
+# Anlık sonuçlar
 xc = np.zeros(2)
 xc = x[0]
 fc = f(xi)
 fs = np.zeros(n+1)
 fs[0] = fc
-# Current temperature
+# Anlık sıcaklık
 t = t1
-# DeltaE Average
+# Ortalama Sıcaklık
 DeltaE_avg = 0.0
 for i in range(n):
-    print('Cycle: ' + str(i) + ' with Temperature: ' + str(t))
+    print('Döngü: ' + str(i) + ' nin sıcaklığı: ' + str(t))
     for j in range(m):
-        # Generate new trial points
+        # yeni noktalar
         xi[0] = xc[0] + random.random() - 0.5
         xi[1] = xc[1] + random.random() - 0.5
-        # Clip to upper and lower bounds
+        # alt üst sıçrama
         xi[0] = max(min(xi[0],2.0),-2.0)
         xi[1] = max(min(xi[1],2.0),-2.0)
         DeltaE = abs(f(xi)-fc)
         if (f(xi)>fc):
-            # Initialize DeltaE_avg if a worse solution was found
-            #   on the first iteration
+            # ilk iterasyonda daha kötü bir ortalama bulduysa          #   on the first iteration
             if (i==0 and j==0): DeltaE_avg = DeltaE
-            # objective function is worse
-            # generate probability of acceptance
+          
+            # kabul  fonksiyonu
+        
             p = math.exp(-DeltaE/(DeltaE_avg * t))
-            # determine whether to accept worse point
+            # kötü çözümü edip etmemeyi kararlaştırma
             if (random.random()<p):
-                # accept the worse solution
+                # kabu etme
                 accept = True
             else:
-                # don't accept the worse solution
+                # kabul etmeme
                 accept = False
         else:
-            # objective function is lower, automatically accept
+            # otomatik kabul etme
             accept = True
         if (accept==True):
-            # update currently accepted solution
+            # anlık durumu güncelleme
             xc[0] = xi[0]
             xc[1] = xi[1]
             fc = f(xc)
-            # increment number of accepted solutions
+            # kabul edilen çözüm sayısını ve delta yı arttırma arttırma
             na = na + 1.0
-            # update DeltaE_avg
             DeltaE_avg = (DeltaE_avg * (na-1.0) +  DeltaE) / na
-    # Record the best x values at the end of every cycle
+    # En iyi x değerlerini saklama
     x[i+1][0] = xc[0]
     x[i+1][1] = xc[1]
     fs[i+1] = fc
-    # Lower the temperature for next cycle
+    # Sıradaki döngü için sıcaklığı azalta
     t = frac * t
 
-# print solution
-print('Best solution: ' + str(xc))
-print('Best objective: ' + str(fc))
+# ekrana yazdırma
+print('En iyi çözüm: ' + str(xc))
+print('En iyi hedef: ' + str(fc))
 
 """plt.plot(x[:,0],x[:,1],'y-o')
 plt.savefig('contour.png')"""
@@ -111,7 +108,8 @@ ax2.plot(x[:,0],'b.-')
 ax2.plot(x[:,1],'g--')
 ax2.legend(['x1','x2'])
 
-# Save the figure as a PNG
+# PNG olarak saklama
 plt.savefig('iterations.png')
 
 plt.show()
+
